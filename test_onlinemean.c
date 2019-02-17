06c2574520f7f7522d31ebc58686d0e3e0f0ea2f
dizcza <dizcza@gmail.com>
2019-02-17 11:42:21 +0200

    fixed typo

--- test_onlinemean.c
+++ test_onlinemean.c
@@ -12,7 +12,7 @@
 #include "onlinemean.h"
 
 #define ONLINEMEAN_TEST_SIZE 100
-#define ONLINEMEAN_ESP (1e-7)
+#define ONLINEMEAN_EPS (1e-7)
 
 
 void OnlineMean_Test() {
@@ -37,8 +37,8 @@ void OnlineMean_Test() {
 	float std = sqrt(var / ONLINEMEAN_TEST_SIZE);
 #endif  /* UNBIASED_ESTIMATOR */
 	float std_online = OnlineMean_GetStd(&oMean);
-	assert(fabs(mean - oMean.mean) < ONLINEMEAN_ESP);
-	assert(fabs(std - std_online) < ONLINEMEAN_ESP);
+	assert(fabs(mean - oMean.mean) < ONLINEMEAN_EPS);
+	assert(fabs(std - std_online) < ONLINEMEAN_EPS);
 }
 
 
